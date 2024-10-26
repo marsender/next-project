4967cf870078e7274bdf4d0ed195af52758b32ba
marsender <didier.corbiere@opale-concept.com>
2024-10-26 19:38:57 +0200

    Add Directus REF #1

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -3,13 +3,24 @@ import { createDirectus, rest } from '@directus/sdk'
 
 const { publicRuntimeConfig, serverRuntimeConfig } = getConfig()
 const { url } = publicRuntimeConfig
-//const { email, password, token } = serverRuntimeConfig
-console.log('Directus url: %s', url)
+const { email, password, token } = serverRuntimeConfig
+//console.log('Directus url: %s', url)
 
 const directus = createDirectus(url).with(
 	rest({
 		onRequest: (options) => ({ ...options, cache: 'no-store' }),
 	})
 )
+
+export async function getDirectusClient() {
+	//console.log('Email: %s', email)
+	if (email && password) {
+		await directus.auth.login({ email, password })
+	} else if (token) {
+		await directus.auth.static(token)
+	}
+
+	return directus
+}
 
 export default directus
