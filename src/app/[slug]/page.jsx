4e6011237ee2467f2e47923172682618c5d94f4c
marsender <didier.corbiere@opale-concept.com>
2024-10-26 19:39:20 +0200

    Add pages REF #2

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,23 @@
+import directus from '@/lib/directus'
+import { notFound } from 'next/navigation'
+import { readItem } from '@directus/sdk'
+
+async function getPage(slug) {
+	try {
+		const page = await directus.request(readItem('pages', slug))
+		return page
+	} catch (error) {
+		notFound()
+	}
+}
+
+export default async function DynamicPage({ params }) {
+	const { slug } = await params
+	const page = await getPage(slug)
+	return (
+		<div>
+			<h1>{page.title}</h1>
+			<div dangerouslySetInnerHTML={{ __html: page.content }}></div>
+		</div>
+	)
+}
