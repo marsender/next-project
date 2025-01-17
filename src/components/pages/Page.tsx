80cb3926deffc62e3f9006084ea932fd94ca25e7
marsender <didier.corbiere@opale-concept.com>
2025-01-17 07:07:09 +0100

    Add padding

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -7,7 +7,7 @@ const Page = ({ page }: any) => {
 		<div className="flex-1 flex flex-col h-full">
 			<section className="bg-white flex flex-1 justify-center">
 				<div className="p-4 m-auto max-w-5xl pt-20 pb-20 text-gray-900 w-full h-full flex max-lg:flex-col">
-					<section className="flex justify-center lg:text-start text-center flex-col flex-1 h-full">
+					<section className="pr-8 flex justify-center lg:text-start text-center flex-col flex-1 h-full">
 						<h1 className="text-4xl xl:text-5xl font-black leading-normal">{page.title}</h1>
 						<div dangerouslySetInnerHTML={{ __html: page.content }}></div>
 					</section>
