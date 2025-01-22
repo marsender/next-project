ddf742fb8ef68263f09e5e68f684c4b7e2d8a6e9
marsender <didier.corbiere@opale-concept.com>
2025-01-22 16:38:17 +0100

    fix incorrect layout for app REF #13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,5 @@ type RootProps = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: RootProps) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	)
+	return children
 }
