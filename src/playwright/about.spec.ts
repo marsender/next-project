096c808070d2dda2679ed1060bd8b6aba86b3314
marsender <didier.corbiere@opale-concept.com>
2025-02-07 13:27:41 +0100

    Playwright end-to-end tests for heading and link REF #21

--- src/playwright/about.spec.ts
+++ src/playwright/about.spec.ts
@@ -7,6 +7,8 @@ test.describe('about page', () => {
 	})
 	test('search text in fr page', async ({ page }) => {
 		await page.goto('/fr/about')
-		await expect(page.getByText('À propos fr')).toBeVisible()
+		await expect(page.getByRole('heading', { name: 'À propos fr' })).toBeVisible()
+		await expect(page.getByRole('link', { name: 'À propos' })).toBeVisible()
+		await expect(page.getByText('Wysiwyg FR')).toBeVisible()
 	})
 })
