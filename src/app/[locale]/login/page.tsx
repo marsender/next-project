addeee383bdac3247ae22b32f7f785af724110c0
marsender <didier.corbiere@opale-concept.com>
2025-02-09 14:31:44 +0100

    Add layout to login page

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,11 +1,23 @@
+import { getTranslations } from 'next-intl/server'
+
+import PageLayout from '@/components/pages/PageLayout'
 import LoginForm from './form'
 
 export const dynamic = 'force-dynamic'
 
-export default async function LoginPage() {
+type Props = {
+	params: Promise<{ locale: string }>
+}
+
+export default async function LoginPage({ params }: Props) {
+	const { locale } = await params
+
+	const t = await getTranslations({ locale, namespace: 'LoginPage' })
 	return (
-		<div>
-			<LoginForm />
-		</div>
+		<PageLayout title={t('title')}>
+			<div className="max-w-[490px]">
+				<LoginForm />
+			</div>
+		</PageLayout>
 	)
 }
