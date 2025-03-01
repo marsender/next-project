e324aed2bf5a6b7a389128673ea343e4b3798f38
marsender <didier.corbiere@opale-concept.com>
2025-03-01 18:27:09 +0100

    Playwright tests REF #29

--- src/playwright/about.spec.ts
+++ src/playwright/about.spec.ts
@@ -9,6 +9,6 @@ test.describe('about page', () => {
 		await page.goto('/fr/about')
 		await expect(page.getByRole('heading', { name: 'À propos fr' })).toBeVisible()
 		await expect(page.getByRole('link', { name: 'À propos' })).toBeVisible()
-		await expect(page.getByText('Wysiwyg GR')).toBeVisible()
+		await expect(page.getByText('Wysiwyg FR')).toBeVisible()
 	})
 })
