91f46e691566e5c74c8f9d07fb06339419262a49
marsender <didier.corbiere@opale-concept.com>
2025-03-31 13:51:21 +0200

    Fix playwright errors

---
+++ src/playwright/home.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('about page', () => {
+	test('search text in en page', async ({ page }) => {
+		await page.goto('/en')
+		await expect(page.getByRole('heading', { name: 'Web Developer' })).toBeVisible()
+		await expect(page.getByRole('link', { name: 'Contact me' })).toBeVisible()
+		await expect(page.getByText('Hosting')).toBeVisible()
+	})
+	test('search text in fr page', async ({ page }) => {
+		await page.goto('/fr')
+		await expect(page.getByRole('heading', { name: 'Développeur Web' })).toBeVisible()
+		await expect(page.getByRole('link', { name: 'Contactez-moi' })).toBeVisible()
+		await expect(page.getByText('Hébergement')).toBeVisible()
+	})
+})
