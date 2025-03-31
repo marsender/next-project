6a7fb1dec376dc885cb6634308b99f7c77256bb5
marsender <didier.corbiere@opale-concept.com>
2025-03-31 11:58:44 +0200

    Add vince analytics REF #34

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -47,7 +47,7 @@ export default async function BaseLayout({ children, locale }: Props) {
 
 	return (
 		<html lang={locale}>
-			<Script defer data-domain="opale.localhost" src="http://localhost:3012/js/script.js" strategy="afterInteractive" />
+			{process.env.VINCEANALYTICS_URL?.length && <Script defer data-domain="opale-concept.com" src={process.env.VINCEANALYTICS_URL} strategy="afterInteractive" />}
 			<body>
 				<NextIntlClientProvider messages={messages}>
 					<Providers session={session}>
