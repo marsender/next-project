873726a9e284db468766422d551815790fe24524
marsender <didier.corbiere@opale-concept.com>
2025-04-02 09:00:38 +0200

    Add doc for favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -16,41 +16,11 @@ type RootProps = {
 	params: Promise<{ locale: string }>
 }
 
-/*
-const title = `Opale-concept`
-const description = 'Teams knoledge'
-
-export const metadata: Metadata = {
-	title: {
-		default: title,
-		template: '%s | Digest.club',
-	},
-	description,
-	twitter: {
-		card: 'summary_large_image',
-		title,
-		description,
-		images: [`${process.env.NEXT_PUBLIC_URL}/vercel.svg`],
-	},
-	openGraph: {
-		title,
-		description,
-		url: process.env.NEXT_PUBLIC_URL,
-		siteName: title,
-		images: [
-			{
-				url: `${process.env.NEXT_PUBLIC_URL}/vercel.svg`,
-				width: 1155,
-				height: 1000,
-			},
-		],
-		locale: 'en-US',
-		type: 'website',
-	},
-}
-*/
-
-// Use either metadata or generateMetadata
+/**
+ * Use either metadata or generateMetadata
+ *
+ * For favicon, see https://stackoverflow.com/questions/75674866/adding-favicon-to-nextjs-app-router-application
+ */
 export async function generateMetadata({ params }: MetadataProps) {
 	const { locale } = await params
 	const languageCode = await getLanguageCode()
