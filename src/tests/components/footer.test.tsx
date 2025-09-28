f7cd42dd4f19c483289c3833b414e0a055b17fb6
marsender <didier.corbiere@opale-concept.com>
2025-09-28 20:37:54 +0200

    fix window.matchMedia is not a function

--- src/tests/components/footer.test.tsx
+++ src/tests/components/footer.test.tsx
@@ -4,6 +4,22 @@ import { NextIntlClientProvider } from 'next-intl'
 import Footer from '@/components/layout/Footer'
 
 describe('Footer', () => {
+	beforeAll(() => {
+		Object.defineProperty(window, 'matchMedia', {
+			writable: true,
+			value: (query: string) => ({
+				matches: false,
+				media: query,
+				onchange: null,
+				addListener: () => {},
+				removeListener: () => {},
+				addEventListener: () => {},
+				removeEventListener: () => {},
+				dispatchEvent: () => false,
+			}),
+		})
+	})
+
 	test('renders the footer', () => {
 		// Mock translations
 		const messages = {
@@ -15,7 +31,7 @@ describe('Footer', () => {
 		render(
 			<NextIntlClientProvider locale="en" messages={messages}>
 				<Footer />
-			</NextIntlClientProvider>
+			</NextIntlClientProvider>,
 		)
 
 		expect(screen.getByText('GitHub')).toBeInTheDocument()
