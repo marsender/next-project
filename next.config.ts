24c5a006dc858a8d804072b0b3590cf738dcc7d9
marsender <didier.corbiere@opale-concept.com>
2025-10-23 20:18:56 +0200

    Remove deprecated config options

--- next.config.ts
+++ next.config.ts
@@ -6,14 +6,14 @@ const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
-	publicRuntimeConfig: {
-		url: process.env.DIRECTUS_URL,
-	},
-	serverRuntimeConfig: {
-		email: process.env.DIRECTUS_EMAIL,
-		password: process.env.DIRECTUS_PASSWORD,
-		// token: process.env.DIRECTUS_STATIC_TOKEN,
-	},
+	// publicRuntimeConfig: {
+	// 	url: process.env.DIRECTUS_URL,
+	// },
+	// serverRuntimeConfig: {
+	// 	email: process.env.DIRECTUS_EMAIL,
+	// 	password: process.env.DIRECTUS_PASSWORD,
+	// 	// token: process.env.DIRECTUS_STATIC_TOKEN,
+	// },
 	// Allow next image to load directus assets
 	images: {
 		remotePatterns: [
