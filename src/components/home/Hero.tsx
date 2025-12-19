e8713f30c406955620766ec853ecb24f2dcdcf80
marsender <didier.corbiere@opale-concept.com>
2025-12-18 22:48:32 -1000

    Remove cal.com link

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -19,9 +19,11 @@ export default async function Hero({ isConnected }: { isConnected: boolean }) {
 				<h1 className="mb-4 text-4xl leading-normal font-black xl:text-5xl">{global.title}</h1>
 				<h2 className="mb-4 text-2xl font-[300]">{global.description}</h2>
 				<div className="flex justify-center gap-4 pt-10 lg:justify-start">
-					{isConnected ? null : (
+					{false && isConnected ? null : (
 						<CustomLink
-							href="https://cal.opale-concept.com/didier.corbiere/meeting"
+							// Calcom is disabled
+							// https://cal.opale-concept.com/didier.corbiere/meeting
+							href=""
 							size="lg"
 							variant="solid"
 							icon={<User size={48} />}
